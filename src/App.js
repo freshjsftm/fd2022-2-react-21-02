6182a34a490e4ad38be02fbabd0c909e9539324a
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-03-03 13:45:04 +0200

    Add SASS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Calendar from "./components/Calendar";
+import Ciao from "./components/CiaoSection/Ciao";
 
 const App = (props) => {
   return (
     <>
       <Calendar />
+      <Ciao classStyle="welcome" name="Brad" id={1}/>
     </>
   );
 };
--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,31 +1,41 @@
 import React, { Component } from "react";
 import { format, add, startOfMonth } from "date-fns";
+import styles from "./Calendar.module.scss";
 
 class Calendar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: new Date(2005,4,2),
+      date: new Date(2005, 4, 2),
     };
   }
 
   addDay = () => {
     const { date } = this.state;
-    this.setState({ date: add(date, {days:1}) });
+    this.setState({ date: add(date, { days: 1 }) });
   };
   addMonth = () => {
     const { date } = this.state;
-    this.setState({ date: add(date, {months: 1}) });
+    this.setState({ date: add(date, { months: 1 }) });
   };
 
   render() {
     const { date } = this.state;
     return (
-      <div>
-        <p>день тижня з якого почався місяць: {format(startOfMonth(date),"EEEE")}</p>
-        <p>{format(date, "'Date:' EEEE, dd LLLL 'Time:' HH:mm:ss")}</p>
-        <button onClick={this.addDay}>add 1 day</button>
-        <button onClick={this.addMonth}>add 1 month</button>
+      <div className={styles.container}>
+        <h3 className={styles.content}>
+          день тижня з якого почався місяць:
+          {format(startOfMonth(date), "EEEE")}
+        </h3>
+        <p className={styles.content}>
+          {format(date, "'Date:' EEEE, dd LLLL 'Time:' HH:mm:ss")}
+        </p>
+        <button className={styles.btn} onClick={this.addDay}>
+          add 1 day
+        </button>
+        <button className={styles.btn} onClick={this.addMonth}>
+          add 1 month
+        </button>
       </div>
     );
   }

9893b918b20c986f7a4d78d5fe61fac43afcce52
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-03-06 16:05:06 +0200

    Add UsersLoader

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
 import "./App.css";
-const ImageWrapper = ()=>{}
+import UsersLoader from "./components/UsersLoader";
+
 const App = (props) => {
   return (
     <>
-      <ImageWrapper
-        width="500px"
-        height="300px"
-        border-radius="50%"
-        onClick={()=>{}}
-        title='sea' //може бути відсутнім
-        tabIndex={5} //може бути відсутнім
-      >
-        <img
-          src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ocean-quotes-index-1624414741.jpg"
-          alt="sea"
-        />
-      </ImageWrapper>
+      <UsersLoader />
     </>
   );
 };

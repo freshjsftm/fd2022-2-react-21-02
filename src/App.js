7600da39304d4eee0cd045e534e005039561f5c8
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-03-09 14:49:34 +0200

    Add example BOM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
-import UsersLoader from "./components/UsersLoader";
 
 const App = (props) => {
   return (
     <>
-      <UsersLoader />
+      <button onClick={()=>window.close()}>close</button>
+      <button onClick={()=>window.open('https://www.w3schools.com/')}>open w3schools</button>
+      <hr /><hr />
+      <button onClick={()=>window.location.assign('https://www.w3schools.com/')}>asign</button>
+      <button onClick={()=>window.location.replace('https://www.google.com/')}>replace</button>
+      <button onClick={()=>window.location.reload()}>reload</button>
     </>
   );
 };

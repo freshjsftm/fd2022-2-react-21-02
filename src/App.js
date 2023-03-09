47ab210ff763ae2da3637bcf1e646cbcb0de3ead
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-03-09 16:01:05 +0200

    Add WindowSizes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
+import WindowSizes from "./components/WindowSizes";
 
 const App = (props) => {
   return (
     <>
-      <button onClick={()=>window.close()}>close</button>
-      <button onClick={()=>window.open('https://www.w3schools.com/')}>open w3schools</button>
-      <hr /><hr />
-      <button onClick={()=>window.location.assign('https://www.w3schools.com/')}>asign</button>
-      <button onClick={()=>window.location.replace('https://www.google.com/')}>replace</button>
-      <button onClick={()=>window.location.reload()}>reload</button>
+      <WindowSizes />
     </>
   );
 };

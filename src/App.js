46bec12e1897e4c484325c72cba110bea4579a78
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-03-09 16:22:58 +0200

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
 import "./App.css";
-import WindowSizes from "./components/WindowSizes";
+
+const Home = ()=> <h1>Home</h1>;
+const About = ()=> <h1>About</h1>;
+const Contacts = ()=> <h1>Contacts</h1>;
+const NotFound = ()=> <h1>404</h1>;
 
 const App = (props) => {
   return (
-    <>
-      <WindowSizes />
-    </>
+  <>    
+    <BrowserRouter>
+    <nav>
+      <ul>
+        <li><NavLink to='/'>home</NavLink></li>
+        <li><NavLink to='/about'>about</NavLink></li>
+        <li><NavLink to='/contacts'>contacts</NavLink></li>
+      </ul>
+    </nav>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contacts" element={<Contacts />}/>
+        <Route path="*" element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
+  </>
   );
 };
 
